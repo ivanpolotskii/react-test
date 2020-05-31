4ff4997014eb46b162e090d104daa96d8c69a83a
mentalist01 <58345789+mentalist01@users.noreply.github.com>
2020-05-31 20:56:56 +0300

    Updates

---
+++ precache-manifest.4372c1553a93731e30b5e3fbdd57593c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a4dfcaeca872eb55db90373249bfe0a",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "e2f7a705353fbb445a8e",
+    "url": "/react-test/static/css/main.106dfc8e.chunk.css"
+  },
+  {
+    "revision": "92e09b19da6dddc12f24",
+    "url": "/react-test/static/js/2.546dfc46.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-test/static/js/2.546dfc46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2f7a705353fbb445a8e",
+    "url": "/react-test/static/js/main.2d2f4a60.chunk.js"
+  },
+  {
+    "revision": "1ccedde05d2608f27700",
+    "url": "/react-test/static/js/runtime-main.92093d93.js"
+  }
+]);
